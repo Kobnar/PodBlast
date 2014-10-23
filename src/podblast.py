24b9eed654ff8f6c2592fed63d64fe5ff98285a2
Konrad R.K. Ludwig <konradicus.ludwidicus.maximus@gmail.com>
2014-10-23 20:19:31 +0000

    Working in Linux.

--- src/podblast.py
+++ src/podblast.py
@@ -31,7 +31,7 @@ import gst
 # A podcast episode:
 #------------------------------------------------------------------------------#
 
-class Episode:
+class Episode(object):
     def __init__(self, episode_source):
         # Defines episode data:
         self.url = episode_source.link
@@ -39,18 +39,17 @@ class Episode:
         self.description = episode_source.description
         self.dtg_published = episode_source.published_parsed
         # Compiles a list of embeded media urls:
-        self.media_urls = []
+        self.media = []
         for media_source in episode_source.media_content:
-            media.append(media_source['url'])
+            self.media.append(media_source['url'])
         # Defines episode metadata:
-        self.valid = episode_source.valid
         self.downloaded = episode_source.downloaded
 
 #------------------------------------------------------------------------------#
 # A podcast (aka: feed):
 #------------------------------------------------------------------------------#
 
-class Feed:
+class Feed(object):
     def __init__(self, feed_source):
         # Extracts feed data:
         self.url = feed_source.url
@@ -59,7 +58,7 @@ class Feed:
         # Compiles a list of episodes:
         self.episodes = []
         for episode_source in feed_source.entries:
-            self.episodes.append(Episode(feed_url, episode_source))
+            self.episodes.append(Episode(episode_source))
         # Extracts feed metadata:
         self.valid = feed_source.valid
         self.subscribed = feed_source.subscribed
@@ -68,34 +67,9 @@ class Feed:
 # An implementation to save and load PodBlast data:
 #------------------------------------------------------------------------------#
 
-class Database:
+class Database(object):
     def __init__(self):
         self.feeds = []
-        self.feeds = load_feeds()
-
-    #--------------------------------------------------------------------------#
-    #     The following two classes establish default formatting for all our
-    #   CSV files in one location.
-    #--------------------------------------------------------------------------#
-
-    # The default 'csv.writer' format:
-    class CSVWriter:
-        def __init__(self, csvfile):
-            self = csv.writer(
-                    csvfile,
-                    delimiter=',',
-                    quotechar='|',
-                    quoting=csv.QUOTE_MINIMAL
-                    )
-
-    # The default 'csv.reader' format:
-    class CSVReader:
-        def __init__(self, csvfile):
-            self = cvs.reader(
-                    csvfile,
-                    delimiter=',',
-                    quotechar='|'
-                    )
 
     #--------------------------------------------------------------------------#
     #     The following three classes workaround Python's single constructor
@@ -115,8 +89,7 @@ class Database:
             self.published_parsed = csv_row[4]
             self.media_content = []
             # Loads episode metadata:
-            self.valid = csv_row[5]
-            self.downloaded = csv_row[6]
+            self.downloaded = csv_row[5]
 
     # Feed data which has been loaded from a single row in a CVS file:
     class LDFeedSource:
@@ -133,15 +106,22 @@ class Database:
     # Feed data which has been fetched and parsed from a remote url:
     class FHFeedSource:
         def __init__(self, feed_url):
-            self.url = feed_url
-            try:
-                self = feedparser.parse(feed_url)
+            # Tries to parse the podcast feed, prints error if it fails:
+            # try:
+                source = feedparser.parse(feed_url)
+                self.title = source.feed.title
+                self.description = source.feed.description
+                self.entries = source.entries
+                for entry in self.entries:
+                    entry.downloaded = False
                 self.valid = True
                 self.subscribed = True
-            except:
-                print ("Failed to parse feed: " + feed_url)
-                self.valid = False
-                self.subscribed = False
+            # except:
+            #     print ("Failed to parse feed: " + feed_url)
+            #     self.valid = False
+            #     self.subscribed = False
+            # Defines url regardless:
+            self.url = feed_url
 
     #--------------------------------------------------------------------------#
     #     The following three methods load data from the program's various
@@ -151,7 +131,7 @@ class Database:
     # Loads all of the media content urls of a given episode:
     def load_media(self, episode_url):
         with open('media.csv', 'rb') as csvfile:
-            media_reader = CSVReader(csvfile)
+            media_reader = csv.reader(csvfile) #Database.CSVReader(csvfile).reader
             media_content = []
             (media_content.append({ 'url': url }) for url in
                 (media[1] for row in media_reader if row[0] is episode_url))
@@ -160,12 +140,12 @@ class Database:
     # Loads all of the episodes of a given feed:
     def load_episodes(self, feed_url):
         with open('episodes.csv', 'rb') as csvfile:
-            episode_reader = CSVReader(csvfile)
+            episode_reader = csv.reader(csvfile) #Database.CSVReader(csvfile).reader
             entries = []
             for row in (rows for row in episode_reader if row[0] is feed_url):
-                source = LDEpisodeSource(row)
+                source = Database.LDEpisodeSource(row)
                 episode_url = row[1]
-                source.media_content = load_media(episode_url)
+                source.media_content = self.load_media(episode_url)
                 entries.append(
                     Episode(source)
                     )
@@ -174,16 +154,15 @@ class Database:
     # Loads all of the feeds which have been saved by the user:
     def load_feeds(self):
         with open('feeds.csv', 'rb') as csvfile:
-            feed_reader = CSVReader(csvfile)
-            feeds = []
+            feed_reader = csv.reader(csvfile) #Database.CSVReader(csvfile).reader
             for row in feed_reader:
                 feed_url = row[0]
-                source = LDFeedSource(row)
-                source.entries = load_episodes(feed_url)
-                feeds.append(
+                source = Database.LDFeedSource(row)
+                source.entries = self.load_episodes(feed_url)
+                self.feeds.append(
                     Feed(source)
                     )
-            return feeds
+                print (self.feeds)
 
     #--------------------------------------------------------------------------#
     #     The following three methods save the program's current data
@@ -194,42 +173,45 @@ class Database:
     # Saves an episode's media urls to a CVS file:
     def save_media(self, episode):
         with open('media.csv', 'wb') as csvfile:
-            filewriter = CSVWriter(csvfile)
-            for media_url in episode.media_urls:
-                self.filewriter.writerow([
-                    self.url,
+            filewriter = csv.writer(csvfile)
+            for media_url in episode.media:
+                filewriter.writerow([
+                    episode.url,
                     media_url
                     ])
 
     # Saves all of a feed's valid episodes to a CVS file:
     def save_episodes(self, feed):
         with open('episodes.csv', 'wb') as csvfile:
-            filewriter = CSVWriter(csvfile)
-            for episode in (episodes for episode in feed.episodes if episode.valid):
-                self.filewriter.writerow([
+            filewriter = csv.writer(csvfile)
+            for episode in feed.episodes:
+                filewriter.writerow([
                     feed.url,
                     episode.url,
                     episode.title,
                     episode.description,
                     episode.dtg_published,
-                    episode.valid,
                     episode.downloaded
                     ])
-                save_media(episode)
+                self.save_media(episode)
 
     # Saves all registered and valid feeds to a CVS file:
     def save_feeds(self):
-        with open('feeds.csv', 'wb') as csvfile:
-            filewriter = CSVWriter(csvfile)
-            for feed in (feeds for feed in self.feeds if feed.valid):
-                self.filewriter.writerow([
-                    feed.url,
-                    feed.title,
-                    feed.description,
-                    feed.valid,
-                    feed.subscribed
-                    ])
-                save_episodes(feed)
+        if self.feeds:
+            with open('feeds.csv', 'wb') as csvfile:
+                filewriter = csv.writer(csvfile)
+                for feed in self.feeds:
+                    if feed.valid:
+                        filewriter.writerow([
+                            feed.url,
+                            feed.title,
+                            feed.description,
+                            feed.valid,
+                            feed.subscribed
+                            ])
+                        self.save_episodes(feed)
+        else:
+            print ("No feeds to save.")
 
     #--------------------------------------------------------------------------#
     #     The following methods handle feed "registration" (ie: fetching a new
@@ -242,7 +224,7 @@ class Database:
     # If a feed with the given url is found, sets 'subscribed' to 'True',
     #   otherwise prints error to console.
     def subscribe_feed(self, feed_url):
-        feed_search = (feed for feed in self.feeds if feed.url is feed_url)
+        feed_search = [feed for feed in self.feeds if feed.url is feed_url]
         if feed_search:
             for feed in feed_search:
                 feed.subscribed = True
@@ -251,7 +233,7 @@ class Database:
 
     # If a feed with the given url is found, sets 'subscribed' to 'False':
     def unsubscribe_feed(self, feed_url):
-        feed_search = (feed for feed in self.feeds if feed.url is feed_url)
+        feed_search = [feed for feed in self.feeds if feed.url is feed_url]
         if feed_search:
             for feed in feed_search:
                 feed.subscribed = False
@@ -260,21 +242,21 @@ class Database:
 
     # Fetches feed data if the given feed_url is not already registered.
     def register_feed(self, feed_url):
-        feed_search = (feed for feed in self.feeds if feed.url is feed_url)
-        if not feed_search:
-            try:
-                source = FHFeedSource(feed_url)
+        feed_search = [feed for feed in self.feeds if feed.url is feed_url]
+        if feed_search:
+            print ("Feed already registered: " + feed_url)
+        else:
+            # try:
+                source = Database.FHFeedSource(feed_url)
                 self.feeds.append(
                     Feed(source)
                     )
-            except:
-                print ("Failed to register feed: " + feed_url)
-        else:
-            print ("Feed already registered: " + feed_url)
+            # except:
+                # print ("Failed to register feed: " + feed_url)
 
     # Deletes any feed objects with a url matching the given 'feed_url':
     def delete_feed(self, feed_url):
-        feed_search = (feed for feed in self.feeds if feed.url is feed_url)
+        feed_search = [feed for feed in self.feeds if feed.url is feed_url]
         for feed in (feeds for feed in feed_search if feed_search):
             feed_index = self.feeds.index(feed)
             self.feeds.remove(feed_index)
@@ -283,7 +265,7 @@ class Database:
 # An implementation to play podcast episode media using GStreamer:
 #------------------------------------------------------------------------------#
 
-class Player:
+class Player(object):
     def __init__(self):
         # Instantiates the 'GStreamer' player "engine" and defines the current
         #   output sink as 'pulseaudio':
@@ -354,9 +336,9 @@ class Player:
 # The "core" 'Feed' subscription management class:
 #------------------------------------------------------------------------------#
 
-class FeedTracker (Database, Player):
+class PodBlast (Database, Player):
     def __init__(self):
-        try:
-            load_feeds()
-        except:
-            print ("Failed to load feed data.")
+        # try:
+            self.feeds = self.load_feeds()
+        # except:
+        #     print ("Failed to load feeds.")
